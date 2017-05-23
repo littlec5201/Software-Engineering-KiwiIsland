26074b702cd5ed6ac67a8cb0ed3ffdb224454ec3
TheMan33 <funguy3@live.com>
2017-05-23 15:16:08 +1200

    Add files via upload

--- GameModel/Game.java
+++ GameModel/Game.java
@@ -4,6 +4,7 @@ package GameModel;
  *
  * @author Ethan
  */
+import Gui.MainGui;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -14,6 +15,13 @@ import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JOptionPane;
 
 /**
@@ -499,6 +507,18 @@ public class Game {
             Food food = (Food) item;
             // player gets energy boost from food
             player.increaseStamina(food.getEnergy());
+            try {
+                    AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("eatingFood.wav").getAbsoluteFile());
+                    Clip clip = AudioSystem.getClip();
+                    clip.open(audioInputStream);
+                    clip.start();
+                } catch (UnsupportedAudioFileException ex) {
+                    Logger.getLogger(MainGui.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(MainGui.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (LineUnavailableException ex) {
+                    Logger.getLogger(MainGui.class.getName()).log(Level.SEVERE, null, ex);
+                }
             // player has consumed the food: remove from inventory
             player.drop(food);
             // use successful: everybody has to know that
@@ -610,6 +630,18 @@ public class Game {
                 Kiwi kiwi = (Kiwi) occupant;
                 if (!kiwi.counted()) {
                     kiwi.count();
+                    try {
+                        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("kiwiCall.wav").getAbsoluteFile());
+                        Clip clip = AudioSystem.getClip();
+                        clip.open(audioInputStream);
+                        clip.start();
+                    } catch (UnsupportedAudioFileException ex) {
+                        Logger.getLogger(MainGui.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(MainGui.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (LineUnavailableException ex) {
+                        Logger.getLogger(MainGui.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                     //                    Remove kiwi from island
                     if (island.removeOccupant(occupant.getPosition(), occupant)) {
                         System.out.println("Removed kiwi from island");
@@ -988,6 +1020,18 @@ public class Game {
             Occupant occupant = island.getPredator(current);
             //Predator has been trapped so remove
             island.removeOccupant(current, occupant);
+            try {
+                    AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("predatorKill.wav").getAbsoluteFile());
+                    Clip clip = AudioSystem.getClip();
+                    clip.open(audioInputStream);
+                    clip.start();
+                } catch (UnsupportedAudioFileException ex) {
+                    Logger.getLogger(MainGui.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(MainGui.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (LineUnavailableException ex) {
+                    Logger.getLogger(MainGui.class.getName()).log(Level.SEVERE, null, ex);
+                }
             predatorsTrapped++;
         }
         return hadPredator;
---
+++ GameModel/MapCreator.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GameModel;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Ethan
+ */
+public class MapCreator {
+    
+    public MapCreator(){
+        
+    }
+    
+    public String[] initialiseTerrain(){
+        String[] terrain = new String[10];
+        String toAdd = "";
+        for(int i = 0; i < 10; i++){
+            for(int j  = 0; j < 10; j++){
+                Random rand = new Random();
+                int randy = rand.nextInt(5);
+                if(randy == 0){
+                    toAdd += '~';
+                }else if(randy == 1){
+                    toAdd += '.';
+                }else if(randy == 2){
+                    toAdd += '^';
+                }else if(randy == 3){
+                    toAdd += '#';
+                }else if(randy == 4){
+                    toAdd += '*';
+                }
+            }
+            terrain[i] = toAdd;
+            toAdd = "";
+        }
+        return terrain;
+    }
+    
+    public void createMap(){
+        Random rand = new Random();
+        String[] terrain = initialiseTerrain();
+        
+        try {
+            PrintWriter out = new PrintWriter(new FileWriter("IslandData.txt"));
+            out.println("10, 10,");
+            for(int i = 0; i < 10; i++){
+                out.println(terrain[i]+",");
+            }
+            out.println("River Song, 0, 2, 100.0, 10.0, 5.0,");
+            out.println("38,");
+            out.println("E,Sandwich,A nice and healthy sandwich, "+rand.nextInt(10)+", "+rand.nextInt(10)+", 2.0, 1.0, 50.0,");
+            out.println("E,Muesli Bar,A juicy and nutricious muesli bar, "+rand.nextInt(10)+", "+rand.nextInt(10)+", 1.0, 1.0, 50.0,");
+            out.println("E,Apple,A juicy apple, "+rand.nextInt(10)+", "+rand.nextInt(10)+", 2.0, 3.0, 50.0,");
+            out.println("E,Orange Juice,A bottle of juice, "+rand.nextInt(10)+", "+rand.nextInt(10)+", 2.0, 3.0, 50.0,");
+            out.println("T,Trap,A trap for predators, "+rand.nextInt(10)+", "+rand.nextInt(10)+", 1.0, 1.0,");
+            out.println("T,Trap,A trap for predators, "+rand.nextInt(10)+", "+rand.nextInt(10)+", 1.0, 1.0,");
+            out.println("ST,SuperTrap,A SuperTrap for predators, "+rand.nextInt(10)+", "+rand.nextInt(10)+", 1.0, 1.0,");
+            out.println("T,Screwdriver,A screwdriver that is useful for fixing traps, "+rand.nextInt(10)+", "+rand.nextInt(10)+", 0.5, 0.75,");
+            out.println("F,Oystercatcher,A variable oystercatcher sitting on sand, "+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("F,Crab,A scuttling crab,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("F,Fernbird,A shy fernbird, "+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("F,Heron,A white-faced heron, "+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("F,Robin,A black robin, "+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("F,Tui,A singing tui, "+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("F,Fern,A silver fern, "+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("F,Manuka,A flowering manukau, "+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("F,Kauri, A tall kauri, "+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("F,Flax, A common flax, "+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("K,Kiwi,A little spotted kiwi,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("K,Kiwi,A little spotted kiwi,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("K,Kiwi,A large brown kiwi,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("K,Kiwi,A large brown kiwi,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("K,Kiwi,A large brown kiwi,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("K,Kiwi,A large brown kiwi,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("K,Kiwi,A large brown kiwi,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("K,Kiwi,A large brown kiwi,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("K,Kiwi,A large brown kiwi,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("K,Kiwi,A large brown kiwi,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("P,Rat,A Norwegian rat,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("P,Cat,A wild cat,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("P,Rat,A Norwegian rat,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("P,Kiore,A pacific rat,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("P,Stoat,A brown and white stoat,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("P,Possum,A bushy tailed possum,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("P,Possum,A bushy tailed possum,"+rand.nextInt(10)+", "+rand.nextInt(10)+",");
+            out.println("H,Cliff,A fall down a steep rocky cliff,"+rand.nextInt(10)+", "+rand.nextInt(10)+",1.0,");
+            out.println("H,Pond,A fall into a deep pond,"+rand.nextInt(10)+", "+rand.nextInt(10)+",1.0,");
+            out.println("H,Rock,A large falling rock,"+rand.nextInt(10)+", "+rand.nextInt(10)+",0.5,");
+            out.println("H,Sunburn,Too much sun has given you bad sunburn and,"+rand.nextInt(10)+", "+rand.nextInt(10)+",0.3,");
+            out.println("H,Fall,Tripping on roots hurt your ankle and,"+rand.nextInt(10)+", "+rand.nextInt(10)+",0.5,");
+            out.println("H,Cliff,A fall down a small cliff,"+rand.nextInt(10)+", "+rand.nextInt(10)+",0.3,");
+            out.println("H,Broken trap,Your predator trap has broken,"+rand.nextInt(10)+", "+rand.nextInt(10)+",0.0,");
+            
+            out.close();
+            
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(MapCreator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
